400adbcfc5a3e00873a90d10acbf71a5dd063d37
BuildTools <unconfigured@null.spigotmc.org>
2022-01-31 23:50:41 -0300

    First item created

--- src/main/java/com/geremiasz/testmod/TestMod.java
+++ src/main/java/com/geremiasz/testmod/TestMod.java
@@ -1,9 +1,11 @@
 package com.geremiasz.testmod;
 
+import com.geremiasz.testmod.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -26,8 +28,10 @@ public class TestMod
     public static final String MOD_ID = "testmod";
 
     public TestMod() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        eventBus.addListener(this::setup);
+
+        ModItems.register(eventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
